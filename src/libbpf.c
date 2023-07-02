1142d69c3b775087201e1e1aaba20751fcd2e3e0
tomerh <tomerh@sentinelone.com>
2023-07-02 18:27:50 +0300

    src: adjust fcntl symbol constraint for arm64
    Specifically, for amazon-ami-2-arm

--- src/libbpf.c
+++ src/libbpf.c
@@ -66,7 +66,11 @@ __asm__(".symver fmemopen,fmemopen@GLIBC_2.2.5");
 // Customization:
 // was fcntl64@GLIBC_2.28,
 // enforce symbol version as linux agent works with GLIBC_2.19
+#if !defined(aarch64)
 __asm__(".symver fcntl64,fcntl@GLIBC_2.2.5");
+#elif defined(aarch64)
+__asm__(".symver fcntl64,fcntl@GLIBC_2.17");
+#endif
 
 // Customization:
 bool use_default_tracefs_location = true;
